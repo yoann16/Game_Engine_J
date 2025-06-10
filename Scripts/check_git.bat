d92cc217c285edc608e975a65f0d34a733d5e13f
Yoann Joseph <yoannjoseph16062000@gmail.com>
2025-06-11 01:51:26 +0200

    chore : added Git verification
    To find out if the user has Git for the project

---
+++ Scripts/check_git.bat
@@ -0,0 +1,21 @@
+@echo off
+
+echo ========= VERIFICATION DE GIT =========
+echo.
+where git >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Git n'est pas installer ou n'est pas dans le PATH.
+    echo Veuillez installer Git et vous assurer qu'il est accessible depuis la ligne de commande.
+    echo.
+    pause
+    exit /b 1
+
+)
+echo Git trouver dans le PATH.
+echo.
+echo ========= GIT PRET A ETRE UTILISER =========
+pause
+exit /b 0
+
+
+    
