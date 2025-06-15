945831d9f9d978da05e58b872473063d94f18b04
Yoann Joseph <yoannjoseph16062000@gmail.com>
2025-06-15 19:51:14 +0200

    fix : Corrected deletion of root CMakeLists.txt
    There was a problem with the check_vs script, which replaced the root cmakelist with the temporary check_vs cmakelist.

--- Scripts/check_vs.bat
+++ Scripts/check_vs.bat
@@ -1,20 +1,5 @@
 @echo off
 
-@REM echo === TEST SIMPLE ===
-@REM cmake --help >nul 2>&1
-@REM if %errorlevel% equ 0 (
-@REM     echo CMake fonctionne
-@REM ) else (
-@REM     echo Problème avec CMake
-@REM )
-
-@REM echo === GENERATEURS CMAKE DISPONIBLES ===
-@REM cmake --help | findstr "Visual Studio"
-@REM echo ==========================================
-
-@REM pause
-@REM exit /b 0
-
 
 echo ========= VERIFICATION DE VISUAL STUDIO =========
 echo.
@@ -31,8 +16,6 @@ if not exist %VSWHERE_PATH% (
 )
 echo Visual Studio Installer found at %VSWHERE_PATH%
 echo.
-echo === DEBUG - Informations VS installees ===
-%VSWHERE_PATH% -latest -products * -format text
 echo ============================================
 %VSWHERE_PATH% -latest -products * >nul 2>&1
 if %errorlevel% neq 0 (
@@ -44,29 +27,32 @@ if %errorlevel% neq 0 (
 )
 echo Visual Studio found in the PATH.
 echo Test compatible with cmake...
+mkdir temp_test_dir
+cd temp_test_dir
 echo cmake_minimum_required(VERSION %MIN_CMAKE_VERSION%) > CMakeLists.txt
 echo project(TestVS) >> CMakeLists.txt
-echo Test avec Visual Studio 2022...
-cmake -S . -B temp_build_test -G "Visual Studio 17 2022" >nul 2>&1
+cmake -S . -B build_test -G "Visual Studio 17 2022" >nul 2>&1
+
 if %errorlevel% equ 0 (
     echo Visual Studio 2022 compatible avec %MIN_CMAKE_VERSION%.
-    rmdir /s /q temp_build_test
+    rmdir /s /q build_test
 ) else (
     echo Test avec Visual Studio 2019...
-    cmake -S . -B temp_build_test -G "Visual Studio 16 2019" >nul 2>&1
+    cmake -S . -B build_test -G "Visual Studio 16 2019" >nul 2>&1
     if %errorlevel% equ 0 (
         echo Visual Studio 2019 compatible avec %MIN_CMAKE_VERSION%.
-        rmdir /s /q temp_build_test
+        rmdir /s /q build_test
     ) else (
         echo Aucune version Visual Studio compatible trouvee.
         echo Il faut Visual Studio 2019 ou 2022 avec le workload C++.
-        del CMakeLists.txt
+        cd ..
+        rmdir /s /q temp_test_dir
         pause
         exit /b 1
     )
 )
-del CMakeLists.txt
-pause
+cd ..
+rmdir /s /q temp_test_dir
 exit /b 0
 
     
